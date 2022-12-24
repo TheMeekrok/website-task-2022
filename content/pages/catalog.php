c0efb7a360d67be63f436e85be914b977b69cd3a
Artyom Kurpas <minicoal@gmail.com>
2022-12-24 16:52:26 +1000

    Merge with pages-layout

--- content/pages/catalog.php
+++ content/pages/catalog.php
@@ -17,8 +17,6 @@ function cut_string($string, $max_symbols) {
 }
 
 $images_directory = "./content/images/image_product/ ";
-<<<<<<< HEAD
-=======
 
 $query_category = mysqli_query($connect, "SELECT DISTINCT `category` FROM `products`");
 $category_arr = mysqli_fetch_all($query_category);
@@ -35,33 +33,10 @@ $categors = $_GET['category'];
 if (isset($categors)){
     $flag = true;
 }
->>>>>>> 43b8992ea4cea9438603279bfd3e50bee96e0117
 
 foreach ($products as $prod) {
     $title = cut_string($prod[1], 100);
     $description = cut_string($prod[3], 300);
-<<<<<<< HEAD
-?>
-
-<div class="container-flex product">
-    <img class="product-image __product-block" src="<?=$images_directory.$prod[4]?>"/>
-    <div class="__product-block container-flex product-info __relative">
-        <div class="__space-40"></div>
-        <h4 class="product-title"><?= $title ?></h4>
-        <h5 class="product-description"><?= $description ?></h5>
-        <h5 class="product-rating">Рейтинг <?= $prod[5] ?></h5>
-    </div>
-    <div class="container-flex product-button-sect __product-block">
-        <h4 class="product-price"><?= $prod[2] ?>₽</h4>
-        <form class="button add-to-cart-button" action = "./content/scripts/php/cart/add_to_cart.php" method="post">
-            <input type="text" name="id" value="<?=$prod[0]?>" style="display:none">
-            <button  class="button add-to-cart-button" type="submit">В корзину</button>
-        </form>
-    </div>
-</div>
-<?php } ?>
-<div class="__space-40"></div>
-=======
     if ($flag == true){
         if ($prod[7] == $categors){
         ?>
@@ -103,4 +78,3 @@ foreach ($products as $prod) {
     <?php }}
      ?>
 <div class="__space-40"></div>           
->>>>>>> 43b8992ea4cea9438603279bfd3e50bee96e0117
