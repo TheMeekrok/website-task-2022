c8e6588028664ab256f36a73e835137a6551251c
Immertion <ezepiz@mail.ru>
2022-12-26 18:18:49 +1000

    fix register

--- content/scripts/php/register_user/create_user.php
+++ content/scripts/php/register_user/create_user.php
@@ -9,17 +9,19 @@ $password_r = hash('sha512', $_POST['password_repeat']);
 
 $query = mysqli_query($connect, "SELECT `login` FROM `users` WHERE `users`.`login` = '$login';");
 $login_cor = mysqli_fetch_all($query);
+print_r($login_cor);
 
-if ($password != $password_r){
+
+if (isset($login_cor)){
+    if ($password != $password_r){
+        header('Location: ..\..\..\..\index.php?page=register');
+    }
+    else{
     header('Location: ..\..\..\..\index.php?page=register');
-}
-else if (empty($login_cor)){
-    header('Location: ..\..\..\..\index.php?page=register');
-}
-else{
     mysqli_query($connect, " INSERT INTO `users` (`id`, `login`, `password`, `privilege`,`first_name`,`second_name`,`date`,`phone`,`mail`) 
     VALUES (NULL, '$login', '$password', NULL, NULL, NULL, NULL, NULL, NULL);");
     header('Location: ..\..\..\..\index.php');
+    }
 }
 
 ?>
